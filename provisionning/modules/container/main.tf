43db88ba28d94d0b6624e5646d1d9f22604e11fe
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-07-08 18:30:55 +0100

    inventory update

--- provisionning/modules/container/main.tf
+++ provisionning/modules/container/main.tf
@@ -30,10 +30,32 @@ resource "docker_container" "container" {
 
   # save the ip of container in an inventory file 
   provisioner "local-exec" {
-    command = "echo ansible_workstation${count.index} ansible_host=${element(["172.20.0.10", "172.20.0.11", "172.20.0.12"], count.index)} ansible_user=techuser ansible_ssh_private_key_file=~/Documents/worketyamo/terraform-docker/provisionning/idkey  host_key_checking = False >> ~/Documents/worketyamo/terraform-docker/inventory.ini"
+    command = "echo ansible_workstation${count.index} ansible_host=${element(["172.20.0.10", "172.20.0.11", "172.20.0.12"], count.index)} ansible_user=techuser ansible_ssh_private_key_file=~/Documents/worketyamo/terraform-docker/provisionning/idkey >> ~/Documents/worketyamo/terraform-docker/inventory.ini"
   }
 
   # depends_on = [ var.image_infos ]
 }
 
 
+resource "terraform_data" "createa-sommefile" {
+  provisioner "local-exec" {
+    command = <<-EOT
+    echo "[defaults]" > ~/Documents/worketyamo/terraform-docker/ansible.cfg
+    echo "host_key_checking = False" >> ~/Documents/worketyamo/terraform-docker/ansible.cfg
+
+
+
+    EOT
+  }
+}
+
+
+  resource "terraform_data" "delete-sommefile" {
+    provisioner "local-exec" {
+      when = destroy
+      command = <<-EOT
+      rm -f ~/Documents/worketyamo/terraform-docker/ansible.cfg
+      rm -f ~/Documents/worketyamo/terraform-docker/inventory.ini
+      EOT
+    }
+  }
